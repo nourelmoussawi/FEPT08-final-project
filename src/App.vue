aebeeff9f547366270a29f2fe68e48baaffd70ad
Nour El Moussawi <nourelmoussawi@outlook.com>
2024-12-03 10:47:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-8xl font-black text-emerald-800">{{ title }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "Holaa",
+    };
+  },
+};
+</script>
+<style></style>
